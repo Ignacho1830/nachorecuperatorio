e9d673a97ade2f82495469f91b1fe881a012c942
Ignacho1830 <163877287+Ignacho1830@users.noreply.github.com>
2024-12-06 11:42:43 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const inputBusqueda = document.getElementById('search-input');
+    const botonesVet = document.querySelectorAll('.Vet-locations button');
+    const searchResults = [];
+
+    inputBusqueda.addEventListener('input', function() {
+        filtroVetLocalidad();
+    });
+
+    function filtroVetLocalidad() {
+        const texto = inputBusqueda.value.toLowerCase();
+        botonesVet.forEach(boton => {
+            const localidad = boton.getAttribute('data-localidad').toLowerCase();
+            if (localidad.includes(texto)) {
+                boton.style.display = '';
+            } else { 
+                boton.style.display = 'none';
+            }
+        });  
+    }
+});
